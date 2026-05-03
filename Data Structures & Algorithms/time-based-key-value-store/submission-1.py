1f3946743084977eb9e9efd88f9431c879cc9182
Gianna Blessing Ishema <122178308+BlessingGianna7@users.noreply.github.com>
2026-05-03 00:33:35 -0400

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,26 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list)   
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = "" 
+        l = 0
+        r = len(self.store[key]) - 1
+        values = self.store[key]
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if values[m][1] > timestamp:
+                r = m - 1
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+      
+
+    
+        return res
